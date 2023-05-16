bf3d8df69a649882eb95125e4936f6c45c12ce27
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-05-16 16:54:59 +0800

    feat: ts-node

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,24 @@
-import express from "express"
+import path from "path"
 
+import express, { Express } from "express"
+import minimist from "minimist"
+import * as dotenv from "dotenv"
+
+
+const args = minimist(process.argv.slice(2))
 function main() {
-    const app = express()
+    useConfig()
+    console.log(process.env.PORT)
+    const app: Express = express()
     const port = 8000
     app.listen(port, () => {
         console.log('server starting')
+    })
+}
+
+export const useConfig = () => {
+    return dotenv.config({
+        path: path.resolve(__dirname, `./.${args.env}.env`)
     })
 }
 
